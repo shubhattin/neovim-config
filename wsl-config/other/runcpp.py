3d737a03535e1064582cd0ac4fb8402a5aba3db4
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2023-09-09 11:11:00 +0530

    fixed c++ runner

--- wsl-config/other/runcpp.py
+++ wsl-config/other/runcpp.py
@@ -11,6 +11,12 @@ if len(sh.argv) == 0:
     print("Usage: runcpp <filename>")
     exit()
 
+
+def delete_file(fl: str):
+    if os.path.exists(fl):
+        os.remove(fl)
+
+
 if sh.argv[0] == "--install":
     if sh.IS_WINDOWS:
         py_path = os.path.dirname(sys.executable) + r"\Scripts"
@@ -19,34 +25,35 @@ if sh.argv[0] == "--install":
         exit()
     elif sh.IS_LINUX:
         pth = os.path.realpath(__file__)
-        inst_path = sh.home() + "/.local/bin/runcpp"
-        sh.copy_file(pth, inst_path)
-        sh.cmd(f"chmod +x {inst_path}")
+        INST_PATH = sh.home() + "/.local/bin/runcpp"
+        sh.copy_file(pth, INST_PATH)
+        sh.cmd(f"chmod +x {INST_PATH}")
         print("Installed as 'runcpp'")
         exit()
 
-nm = sh.argv[0]
+NM = sh.argv[0]
 
-if os.path.isfile(f"{nm}.cpp"):
-    nm = f"{nm}.cpp"
-elif os.path.isfile(f"{nm}.c"):
-    nm = f"{nm}.c"
+if os.path.isfile(f"{NM}.cpp"):
+    NM = f"{NM}.cpp"
+elif os.path.isfile(f"{NM}.c"):
+    NM = f"{NM}.c"
 
-if not nm.endswith(".c") and not nm.endswith(".cpp"):
+if not NM.endswith(".c") and not NM.endswith(".cpp"):
     print("Not a C/C++ file")
     exit()
-if not os.path.isfile(f"{nm}"):
-    print(nm + " not found")
+if not os.path.isfile(f"{NM}"):
+    print(NM + " not found")
     exit()
 
-compiler_name = {"cpp": "g++", "c": "gcc"}[nm.split(".")[-1]]
+compiler_name = {"cpp": "g++", "c": "gcc"}[NM.split(".")[-1]]
 
-pth = nm.split("/")
-parent_path = sh.parent(nm)
+pth = NM.split("/")
+parent_path = sh.parent(NM)
 
+ONLY_NAME = ".".join(pth[-1].split(".")[:-1])
 OUTPUT_EXT = "exe" if sh.IS_WINDOWS else "o"
 compile_data: List[Any] = sh.cmd(
-    f"{compiler_name} {nm} -o {nm}.{OUTPUT_EXT}", display=False
+    f"{compiler_name} {NM} -o {ONLY_NAME}.{OUTPUT_EXT}", display=False
 )
 
 if compile_data[0] != 0:
@@ -55,11 +62,14 @@ if compile_data[0] != 0:
 elif compile_data[0] == 0:
     # Success
     try:
-        cm = ""
-        if len(pth) > 1:
-            cm += f"cd {parent_path}\n"
-        sh.cmd(f"{cm}./{pth[-1]}.{OUTPUT_EXT}", direct=False)
-    except Exception:
-        pass
+        CMD = ""
+        # if len(pth) > 1:
+        # CMD += f"cd {parent_path} && "
+        if sh.IS_LINUX:
+            sh.cmd(f"{CMD}./{ONLY_NAME}.{OUTPUT_EXT}", direct=False)
+        elif sh.IS_WINDOWS:
+            sh.cmd(f"{CMD}.\\{ONLY_NAME}.{OUTPUT_EXT}", direct=False)
+    except Exception as e:
+        print(e)
     finally:
-        sh.delete_file(f"{nm}.{OUTPUT_EXT}")
+        delete_file(f"{ONLY_NAME}.{OUTPUT_EXT}")
